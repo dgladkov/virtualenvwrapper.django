ecd64307326a0e4a05eaf57d7f9e67d8586a055d
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2014-01-11 16:55:54 +0200

    add support for specifying django project template

--- virtualenvwrapper/django_project.py
+++ virtualenvwrapper/django_project.py
@@ -13,12 +13,19 @@ import subprocess
 log = logging.getLogger('virtualenvwrapper.django')
 
 
-def template(args):
+def template(_args):
     """Installs Django and runs django-admin to create a new project.
     """
     project, project_dir = args
     os.chdir(project_dir)
     subprocess.check_call(['pip', 'install', 'django'])
-    log.info('Running "django-admin.py startproject %s"', project)
-    subprocess.check_call(['django-admin.py', 'startproject', project])
+    call_args = ['django-admin.py', 'startroject', project]
+    django_template = os.environ.get('DJANGO_DEFAULT_PROJECT_TEMPLATE')
+    django_template_extensions = os.environ.get('DJANGO_DEFAULT_PROJECT_TEMPLATE_EXTENSIONS')
+    if django_template:
+        call_args.append('--template=%s' % django_template
+    if django_template_extensions:
+        call_args.append('--extenstion=%s' % django_template_extensions)
+    log.info('Running "%s"', ' '.join(call_args))
+    subprocess.check_call(call_args)
     return
