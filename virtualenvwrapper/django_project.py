669292b84401721c1526e564e1915d0f68d37d05
Dmitry Gladkov <dmitry.gladkov@gmail.com>
2014-01-13 14:00:57 +0200

    fix another typo

--- virtualenvwrapper/django_project.py
+++ virtualenvwrapper/django_project.py
@@ -13,19 +13,19 @@ import subprocess
 log = logging.getLogger('virtualenvwrapper.django')
 
 
-def template(_args):
+def template(args):
     """Installs Django and runs django-admin to create a new project.
     """
     project, project_dir = args
     os.chdir(project_dir)
     subprocess.check_call(['pip', 'install', 'django'])
-    call_args = ['django-admin.py', 'startroject', project]
+    call_args = ['django-admin.py', 'startproject', project]
     django_template = os.environ.get('DJANGO_DEFAULT_PROJECT_TEMPLATE')
     django_template_extensions = os.environ.get('DJANGO_DEFAULT_PROJECT_TEMPLATE_EXTENSIONS')
     if django_template:
-        call_args.append('--template=%s' % django_template
+        call_args.append('--template={0}'.format(django_template))
     if django_template_extensions:
-        call_args.append('--extenstion=%s' % django_template_extensions)
+        call_args.append('--extension={0}'.format(django_template_extensions))
     log.info('Running "%s"', ' '.join(call_args))
     subprocess.check_call(call_args)
     return
